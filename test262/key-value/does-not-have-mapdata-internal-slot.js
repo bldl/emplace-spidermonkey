de5aa42fd0ce5db65ba2d64ad8f20f79fe46769e
Sune.Lianes <sune.lianes@student.uib.no>
2024-10-23 13:07:06 +0200

    wrote does-not-have-mapdata-internal-slot.js

---
+++ test262/key-value/does-not-have-mapdata-internal-slot.js
@@ -0,0 +1,32 @@
+// Copyright (C) 2024 Sune Eriksson Lianes. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: pending
+description: >
+    Throws a TypeError if `this` object does not have a [[MapData]] internal slot.
+info: |
+    Map.getOrInsert ( key , value )
+    ...
+    2. Perform ? RequireInternalSLot(M, [[MapData]])
+    ...
+---*/
+
+var map = new Map();
+
+assert.throws(TypeError, function () {
+    map.getOrInsert.call([], 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    map.getOrInsert.call(false, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    map.getOrInsert.call({}, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    map.getOrInsert.call({}, 1, 1);
+});
+
+reportCompare(0, 0);
