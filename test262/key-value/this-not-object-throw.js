504322fc3d967f922f7cea0755c3dc56511b4a90
Sune.Lianes <sune.lianes@student.uib.no>
2024-10-23 09:50:30 +0200

    added test for this-not-object

---
+++ test262/key-value/this-not-object-throw.js
@@ -0,0 +1,43 @@
+// Copyright (C) 2024 Sune Eriksson Lianes. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+/*---
+esid: pending
+description: >
+    Throws a TypeError if `this` is not an Object.
+info: |
+    Map.getOrInsert ( key, value )
+
+    1. Let M be the this value
+    2. Perform ? RequireInternalSlot(M, [[MapData]])
+    ...
+features: [Symbol]
+---*/
+
+var m = new Map();
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call(false, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call(1, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call("", 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call(undefined, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call(null, 1, 1);
+});
+
+assert.throws(TypeError, function () {
+    m.getOrInsert.call(Symbol(), 1, 1);
+});
+
+
+reportCompare(0, 0);
