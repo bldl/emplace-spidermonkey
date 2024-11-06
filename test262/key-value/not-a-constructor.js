23abc83176f7be7842707db06e11468000831ba1
Mathias Ness <mathias1389@gmail.com>
2024-11-06 09:44:17 +0100

    remove old sample cases

--- test262/key-value/not-a-constructor.js
+++
@@ -1,8 +0,0 @@
-
-assert.sameValue(isConstructor(Map.prototype.getOrInsert), false, 'isConstructor(Map.prototype.getOrInsert) must return false');
-
-assert.throws(TypeError, () => {
-    let m = new Map(); new m.getOrInsert();
-});
-
-reportCompare(0, 0);
